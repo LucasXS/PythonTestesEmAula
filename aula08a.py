b69f141cf1b7db4e88378763569e5a309343b7f2
lucas <contato_lucassantos@outlook.com>
2021-02-08 20:21:12 -0300

    Import
    Aprendendo sobre como importar e usar uma biblioteca em Python

---
+++ aula08a.py
@@ -0,0 +1,13 @@
+import math
+import random
+import emoji
+
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A Raiz de {} é igual a {:.3}'.format(num,raiz)) #math.ceil(raiz) arredonda pra cima floor pra baixo
+
+#num = random.randint(1, 100)
+#print(num)
+
+print(emoji.emojize('Olá, Mundo :sunglasses:', use_aliases=True))
+
