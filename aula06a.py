e4f9c94c150ef9e7263f18085d641c93513c8bd4
lucas <contato_lucassantos@outlook.com>
2021-02-07 21:47:09 -0300

    Teste da aula 1 Apenas para aprender a usar o .is

---
+++ aula06a.py
@@ -0,0 +1,20 @@
+#n1 = input('Digite um número: ')
+#print(type(n1))
+
+#n1 = int(input('Digite um número: '))
+#print(type(n1))
+
+n1 = int (input('Digite um valor: '))
+n2 = int (input('Digite um valor: '))
+s = n1 + n2
+#print('A soma entre',n1, 'e', n2,' vale', s) sem o .format
+print('A Soma entre {} e {} vale {}'.format(n1, n2, s)) #com o .format
+
+
+
+
+
+
+
+
+
